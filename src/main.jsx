065d1a839440d1d73fcfc09c83d4ac528323a06b
sanjay-x <sanjayxz2003@gmail.com>
2026-03-03 01:49:12 +0530

    route handling static.json 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
-import { HashRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <ConfigProvider theme={{ token: { colorPrimary: "#C6A969", colorBgContainer: "#FFFFFF", borderRadius: 0 } }}>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#C6A969',
+            colorBgContainer: '#FFFFFF',
+            borderRadius: 0,
+          },
+        }}
+      >
         <App />
       </ConfigProvider>
     </HashRouter>
   </React.StrictMode>
-);
+)
